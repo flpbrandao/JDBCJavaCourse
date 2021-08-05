3227ca2f35c5e54b07018e64a0dbcac67b5d4514
Felipe Oliveira <flpbrandao@hotmail.com>
2021-08-05 16:46:20 -0300

    Updating registries with PreparedStatement

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,50 +10,27 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-
-			// EXAMPLE 1 - normal insertion:
+	
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)", 
-					Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
 
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-
-			// EXAMPLE 2 wwith ID retrieve:
-			//st = conn.prepareStatement(
-			//		"insert into department (Name) values ('D1'),('D2')", 
-			//		Statement.RETURN_GENERATED_KEYS);
-
-			int rowsAffected = st.executeUpdate(); //st.executeUpdate returns an integer number with number of rows affected;
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id: " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		} 
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
